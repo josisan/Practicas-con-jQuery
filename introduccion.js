a77572d23af94ff9bd65d0db39435e9db34bfd63
Josias Sanchez <softdevlearn@gmail.com>
2022-02-27 15:29:58 -0400

    Subiendo practica de jQuery a su respectivo repositorio

---
+++ introduccion.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+    
+    // sintaxis basica para jQuery $(selector).accion();
+
+    // $ - Indicamos que vamos a acceder a jquery.
+    // Selector - Accedemos al elemento que queramos trabajar.
+    // Accion - Lo que queremos hacer con el elemento seleccionado.
+
+    $('h1').hide();
+
+});
+
+//------
+//Esta es otra forma de utilizar = $(document).ready(function())
+$(function(){
+
+});
---
+++ selectores.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){;
+    //$('h1').hide();
+    //$('h1, h2').hide();
+    //$('.encabezado').hide();
+    //$('p#primero').hide();
+    //$('p.primero').hide();
+    $('.primero a').hide();
+});
