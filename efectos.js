e875b562e60a54cb2338082ec6756327d7e71022
Josias Sanchez <softdevlearn@gmail.com>
2022-03-04 11:41:42 -0400

    Entrar

---
+++ efectos.js
@@ -0,0 +1,17 @@
+//$(document).ready(function(){
+    function efecto(){
+       // $(selector).efecto(velocidad, callback)
+
+       //$('.caja').hide(3000, function(){
+       //     alert('Oculto');
+       //});
+
+       //$('.caja').toggle(3000);
+       //$('.caja').fadeIn(1000);
+       //$('.caja').fadeOut(1000);
+       //$('.caja').fadeToggle(1000);
+       //$('.caja').slideDown();
+       //$('.caja').slideUp();
+       $('.caja').slideToggle();
+    }
+//})
